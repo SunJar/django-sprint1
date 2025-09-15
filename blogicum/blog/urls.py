eda762d9eb9e4602842611f9c3f01473b6bf9935
Sanzhar Sharipov <sanzharsharipov@MacBook-Pro-Sanzhar.local>
2025-09-16 00:54:37 +0500

    Добавлены новые файлы проекта, исправлены PEP8 ошибки

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -6,5 +6,9 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.main_page_view, name='index'),
     path('posts/<int:post_id>/', views.details_of_posts, name='post_detail'),
-    path('category/<slug:category_slug>/', views.category_view, name='category_posts'),
+    path(
+        'category/<slug:category_slug>/',
+        views.category_view,
+        name='category_posts',
+    ),
 ]
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
-def AboutView(request: HttpRequest) -> HttpResponse:
-    """
-    страница с описанием проекта.
-    """
+
+def about_view(request: HttpRequest) -> HttpResponse:
+    """Страница с описанием проекта."""
     return render(request, 'about.html')
 
-def RulesView(request: HttpRequest) -> HttpResponse:
-    """
-    страница с правилами проекта.
-    """
+
+def rules_view(request: HttpRequest) -> HttpResponse:
+    """Страница с правилами проекта."""
     return render(request, 'rules.html')
