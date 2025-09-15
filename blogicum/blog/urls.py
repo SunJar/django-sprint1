0dd585f4e3ea4b99677787b307fd839886fadf23
Sanzhar Sharipov <sanzharsharipov@MacBook-Pro-Sanzhar.local>
2025-09-16 03:41:21 +0500

    Исправление ошибок

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
+
 from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.main_page_view, name='index'),
-    path('posts/<int:post_id>/', views.details_of_posts, name='post_detail'),
-    path(
-        'category/<slug:category_slug>/',
-        views.category_view,
-        name='category_posts',
-    ),
+    path('', views.index, name='index'),
+    path('posts/<int:id>/', views.post_detail, name='post_detail'),
+    path('category/<slug:category_slug>/', views.category_posts,
+         name='category_posts'),
 ]
